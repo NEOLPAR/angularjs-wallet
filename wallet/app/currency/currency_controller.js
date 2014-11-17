5ba5c8ca1f20bb1b5cfb92c5e5d8ec75fe42c08c
NEOLPAR <ricardo.berrocal@opendeusto.es>
2014-11-17 11:17:12 +0000

    Prettify currency section
    Prettify currency section and active currency when you want to change
    your currency.
    If you reset the wallet, you will have to choose currency again and
    there isn’t selected currency.

--- wallet/app/currency/currency_controller.js
+++ wallet/app/currency/currency_controller.js
@@ -2,9 +2,11 @@
 
 angular.module('WalletApp.CurrencyView', [])
   .controller('CurrencyController', ['$location', '$scope', 'currencyService', function($location, $scope, currencyService){
-    $scope.wrapperId = "wallet";
-
+    $scope.wrapperId = "currency";
     $scope.currencies = currencyService.getAllCurrencies();
+    currencyService.getUserCurrency().then(function(userCurrency){
+      $scope.userCurrency = userCurrency;
+    });
 
     $scope.chooseCurrency = function(currency){
       currencyService.setUserCurrency(currency).then(
